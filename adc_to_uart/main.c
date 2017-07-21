7e2054f928f2a4408a4d982f806b53a0dda3b26d
LordD98 <lorddelhey@gmail.com>
2017-07-21 12:55:49 +0200

    ADC_to_UART now working

--- adc_to_uart/main.c
+++ adc_to_uart/main.c
@@ -6,7 +6,7 @@
 #include <util/delay.h>
 
 
-int uart_put_byte( FILE* stream, uint8_t byte );
+int uart_put_byte(uint8_t byte, FILE* stream);
 
 FILE mystream = FDEV_SETUP_STREAM( uart_put_byte, NULL, _FDEV_SETUP_WRITE );
 
@@ -18,22 +18,17 @@ int main(void)
 {
     uint16_t adc_value;
     setup();
-
-	stdout = &mystream;
-	stdin = &mystream;
-	
-	printf("Analog-Wert: \n"/*, (adc_value>>2)&0xFF*/);
-    //while(1)
-	//{
-	    //if( ~(PINA & (1<<PINA0)) )
-		//{
-			//adc_value = read_adc();
-			///*adc_value mit printf ausgeben*/
-			//printf("Analog-Wert: "/*, (adc_value>>2)&0xFF*/);
-			///*10ms Warten zum entprellen*/
-			//_delay_ms(50);
-	    //}
-    //}
+    while(1)
+	{
+	    if((~PINA & (1<<PINA0)))
+		{
+			adc_value = read_adc();
+			/*adc_value mit printf ausgeben*/
+			printf("Analog-Wert: %d\n", (adc_value>>2)&0xFF);
+			/*10ms Warten zum entprellen*/
+			_delay_ms(10);
+	    }
+    }
 
     return 0;
 }
@@ -49,6 +44,14 @@ void setup() {
 	UCSRB = (1<<TXEN);
 	UCSRC = (1<<URSEL) | (1<<UCSZ0) | (1<<UCSZ1);
 
+
+	stdout = &mystream;
+	stdin = &mystream;
+	
+	printf("ADC Beispiel mcAG \n");
+	printf("=========================================================================\n");
+
+
 	//Gesamter PORTA als Input
 	DDRA = 0;
 
@@ -61,10 +64,10 @@ void setup() {
 	// * ADC kein Auto Trigger
 	// * ADC Teiler auf 128
 	
-	ADMUX |= (1<<REFS0)|(7<<MUX0);
+	ADMUX = (1<<REFS0)|(7<<MUX0);
 	//SFIOR = 0; //free running mode
-	ADCSRA |= (1<<ADPS0) | (1<<ADPS1) | (1<<ADPS2);
-	ADCSRA |= (1<<ADEN)|(1<<ADSC)|(1<<ADATE);
+	ADCSRA = (1<<ADEN) | (1<<ADPS0) | (1<<ADPS1) | (1<<ADPS2);
+	//ADCSRA |= (1<<ADEN)|(1<<ADSC)|(1<<ADATE);
 }
 
 uint16_t read_adc() {
@@ -74,8 +77,9 @@ uint16_t read_adc() {
 	return ADC;
 }
 
-int uart_put_byte( FILE* stream, uint8_t byte ) {
-	while( !( UCSRA & (1<<UDRE) ) );
+int uart_put_byte(uint8_t byte, FILE* stream)
+{
+	while(!(UCSRA&(1<<UDRE)));
 	UDR = byte;
 	return 0;
 }
